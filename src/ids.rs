7b5e5dfbccf98045697c84a0879326f5f5b98e2e
DevSlashRichie <me@slashrichie.dev>
2025-01-14 00:19:34 -0600

    feat: added payment method domain

--- src/ids.rs
+++ src/ids.rs
@@ -597,6 +597,7 @@ def_id!(TransferReversalId, "trr_");
 def_id!(UsageRecordId, "mbur_");
 def_id!(UsageRecordSummaryId, "urs_" | "sis_");
 def_id!(WebhookEndpointId, "we_");
+def_id!(PaymentMethodDomainId, "pmd_");
 
 impl InvoiceId {
     pub(crate) fn none() -> Self {
